35bd19c4b313c6e0fb4bb27e0d35581c9aada75e
Vanshitasingh <singhvanshita89@gmail.com>
2021-07-06 20:24:49 +0530

    New

--- index.js
+++ index.js
@@ -230,9 +230,9 @@ app.post("/login",(req,res)=>{
     res.sendFile(path.join(__dirname+'/login.html'));
    });
 
-   app.listen(process.env.PORT||3000,function () {
-    console.log("Server is running at port 3000");
-   });
+   app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
    
